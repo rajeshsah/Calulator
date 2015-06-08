a5471e15505920e4e7008c1c45ebc27804929552
Rajesh.Sah <rajesh.sah@urbanladder.com>
2015-06-08 16:32:08 +0530

    Second test case added for parser spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe Parser do
   context "parser" do
-    it "should return 5 when input: add 5" do
+    it "should return ['add','5'] if input is 'add 5'" do
       parser = Parser.new
       expected_array = ['add','5']
       expect(parser.parse("add 5")).to eq(expected_array)
+    end
+    it "should return ['multiply','5'] if input is 'multiply 5'" do
+      parser = Parser.new
+      expected_array = ['multiply','5']
+      expect(parser.parse("multiply 5")).to eq(expected_array)
     end
   end
 
